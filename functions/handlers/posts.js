6f729bb39499fe22497c429cbf11d74d4592b261
adityalpha <adityalpha@gmail.com>
2019-10-09 09:49:24 +0530

    Refactoring and Organizing

---
+++ functions/handlers/posts.js
@@ -0,0 +1,45 @@
+const { db } = require("../util/admin");
+
+exports.getAllPosts = (req, res) => {
+  db.collection("posts")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then(data => {
+      let posts = [];
+      data.forEach(doc => {
+        posts.push({
+          postId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
+        //console.log(doc.id, '=>', doc.data());
+      });
+      return res.json(posts);
+    })
+    .catch(err => {
+      console.log("Error getting documents", err);
+    });
+};
+
+exports.postOnePost = (req, res) => {
+  if (req.body.body.trim() === "") {
+    return res.status(400).json({ body: "Body must not be empty" });
+  }
+
+  const newPost = {
+    body: req.body.body,
+    userHandle: req.user.handle,
+    createdAt: new Date().toISOString() //admin.firestore.Timestamp.fromDate(new Date())
+  };
+
+  db.collection("posts")
+    .add(newPost)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "something went wrong" });
+      console.error(err);
+    });
+};
---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyAS38x1Hd01c29hFDpAwH-Un9VpjIpYZ0A",
+  authDomain: "socialapp-6eee5.firebaseapp.com",
+  databaseURL: "https://socialapp-6eee5.firebaseio.com",
+  projectId: "socialapp-6eee5",
+  storageBucket: "socialapp-6eee5.appspot.com",
+  messagingSenderId: "808144554883",
+  appId: "1:808144554883:web:1d875ad0d7469e208ac4b6",
+  measurementId: "G-B7KFQE8Y72"
+};
